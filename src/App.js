73cd84e828cc7ce4aeea79606a15a1e4231adfcb
SethOberg <seth.a.oberg@gmail.com>
2023-05-03 08:52:00 +0200

    Feat: added routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./Pages/LoginPage";
+import SignUpPage from "./Pages/SignUpPage";
+import TodolistPage from "./Pages/TodolistPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/todos" element={<TodolistPage />} />
+      </Routes>
     </div>
   );
 }
